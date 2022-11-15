d2b38c606ade727f3bd6da15d16dcc78a69d8043
MHindt <75388368+MHindt@users.noreply.github.com>
2022-11-15 12:42:27 -0500

    YouTube API set-up

---
+++ src/apis/youTube.js
@@ -0,0 +1,16 @@
+
+
+
+//data to be sent as params in fetch request
+const data = {
+    part: 'snippet',
+    maxResults: 5,
+    key: key,
+}
+//building the URL 
+let url = 'https://www.googleapis.com/youtube/v3/search';
+url += '?';
+for(let param in data) {
+    url += param + '=' + data[param] + '&'
+}
+export default url;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+//REact uses dotenv under the hood naming convention REACT_KEY_WHATEVERNAME 
+//can access with process.env.REACT_KEY_WHATEVERNAME 
+import url from '../apis/youTube';
 
 class App extends React.Component {
+    
+    onSearchSubmit = async (searchInput) => {
+        console.log(searchInput);
+      const response = await fetch(url  + new URLSearchParams({
+        q: searchInput
+      }))
+      const data = await response.json();
+      console.log(data);
+    }
  render() {
+    console.log(url);
     return <div className='ui container'>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onSearchSubmit}/>
     </div>
  }
 }
