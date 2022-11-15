2a9fe7b5674ecfc2be861d3662171b9a02d61716
MHindt <75388368+MHindt@users.noreply.github.com>
2022-11-15 13:02:18 -0500

    Adding VideoList Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,27 @@ import SearchBar from './SearchBar';
 //REact uses dotenv under the hood naming convention REACT_KEY_WHATEVERNAME 
 //can access with process.env.REACT_KEY_WHATEVERNAME 
 import url from '../apis/youTube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-    
+    state = {
+        videos: []
+    }
     onSearchSubmit = async (searchInput) => {
         console.log(searchInput);
       const response = await fetch(url  + new URLSearchParams({
         q: searchInput
       }))
       const data = await response.json();
-      console.log(data);
+      console.log(data.items);
+      this.setState({ videos: data.items })
     }
  render() {
     console.log(url);
     return <div className='ui container'>
         <SearchBar onFormSubmit={this.onSearchSubmit}/>
+        I have {this.state.videos.length} videos
+        <VideoList />
     </div>
  }
 }
