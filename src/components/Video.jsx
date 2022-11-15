6f54590a6700665067321753cc238cd43684b9e8
MHindt <75388368+MHindt@users.noreply.github.com>
2022-11-15 14:38:49 -0500

    css styling of video list

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Video.css';
 
 const Video = ({ video }) => {
-  return( <div>
-    <img src={video.snippet.thumbnails.medium.url}/>
-    {video.snippet.title}
+  return (
+    <div className='video item'>
+      <img className='ui image' src={video.snippet.thumbnails.medium.url} />
+      <div className='content'>
+        <div className='header'>{video.snippet.title}</div>
+      </div>
     </div>
-  )
+  );
 };
 export default Video;
