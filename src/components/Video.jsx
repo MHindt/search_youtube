5d4c2ab32e9dba0d761ff090a410645c8a6b382e
MHindt <75388368+MHindt@users.noreply.github.com>
2022-11-16 10:06:09 -0500

    styling player page

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Video.css';
 
-const Video = ({ video,  onSelectedVideo }) => {
+const Video = ({ video, onSelectedVideo }) => {
   return (
-    <div onClick= {() => onSelectedVideo(video)}className='video item'>
-      <img className='ui image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
+    <div onClick={() => onSelectedVideo(video)} className='video item'>
+      <img
+        alt={video.snippet.title}
+        className='ui image'
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className='content'>
         <div className='header'>{video.snippet.title}</div>
       </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Video from './Video';
 
-const VideoList = ({ videos, onSelectedVideo }) => { 
-    //console.log(videos.id);
-      const renderList = videos.map((video) => {
-        return <Video key={video.id.videoId} onSelectedVideo={onSelectedVideo} video={video} />
-      })
-       return (
-        <div className='ui relaxed divided list'>
-            {renderList}
-        </div>
-       )
-    
-}
+const VideoList = ({ videos, onSelectedVideo }) => {
+  //console.log(videos.id);
+  const renderList = videos.map((video) => {
+    return (
+      <Video
+        key={video.id.videoId}
+        onSelectedVideo={onSelectedVideo}
+        video={video}
+      />
+    );
+  });
+  return <div className='ui relaxed divided list'>{renderList}</div>;
+};
 export default VideoList;
