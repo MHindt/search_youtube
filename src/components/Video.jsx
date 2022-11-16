c9ee777886601f4b8401ee655b462fbb03a9547b
MHindt <75388368+MHindt@users.noreply.github.com>
2022-11-16 09:36:56 -0500

    adding player for videos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Video.css';
 
-const Video = ({ video }) => {
+const Video = ({ video,  onSelectedVideo }) => {
   return (
-    <div className='video item'>
-      <img className='ui image' src={video.snippet.thumbnails.medium.url} />
+    <div onClick= {() => onSelectedVideo(video)}className='video item'>
+      <img className='ui image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
       <div className='content'>
         <div className='header'>{video.snippet.title}</div>
       </div>
